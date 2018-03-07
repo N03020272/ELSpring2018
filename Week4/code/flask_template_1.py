cf2cdba9ad700c3ec2a21526f361512cbb0089a4
Joseph Adamczyk <adamczyj1@hawkmail.newpaltz.edu>
2018-03-07 11:46:12 -0500

    Added up to week 5 of exercises

---
+++ Week4/code/flask_template_1.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import datetime
+app = Flask(__name__)
+@app.route("/")
+def index():
+	templateData1 = {
+		'title' : 'Index.',
+		'msg' : 'This is the Index'
+		}
+	return render_template('index.html', **templateData1)
+
+@app.route("/time")
+def hello():
+	now = datetime.datetime.now()
+	timeString = now.strftime("%Y-%m-%d %H:%M")
+  	templateData = {
+		'title' : 'HELLO!',
+		'time': timeString
+      		}
+	return render_template('main.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
